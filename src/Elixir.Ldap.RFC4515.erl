182db1435db2f634b38cfa489cb024b944c6a547
Dairon Medina Caro <dairon.medina@gmail.com>
2023-08-04 10:30:16 -0400

    maybe fix dialyzer in OTP 26

--- src/Elixir.Ldap.RFC4515.erl
+++ src/Elixir.Ldap.RFC4515.erl
@@ -2,6 +2,6 @@
 
 -export([to_eldap/1]).
 
--spec to_eldap(binary() | list()) -> {ok, eldap:filter()} | {error, any()}.
+-spec to_eldap(binary() | list()) -> {ok, ldap_rfc4515:eldap_filter()} | {error, any()}.
 to_eldap(String) ->
   ldap_rfc4515:to_eldap(String).
